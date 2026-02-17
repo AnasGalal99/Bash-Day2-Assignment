346123643deafa1f0eea0bbdb086991520e0f4e7
AnasGalal99 <anas49594@gmail.com>
2026-02-17 18:43:07 +0200

    last version

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Required_Age=18
+
+echo "Enter your username:"
+read NAME
+
+echo "Enter your age:"
+read AGE
+
+echo "Username: $NAME"
+echo "Age: $AGE"
+echo "Required Age: $Required_Age"
